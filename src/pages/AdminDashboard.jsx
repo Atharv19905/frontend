ed03ef9606add8b9307b593438fe07524398a533
Atharv19905 <atharvpchougule19@gmail.com>
2026-05-09 00:57:38 +0530

    Update AdminDashboard.jsx

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -149,7 +149,7 @@ export default function AdminDashboard() {
                             onClick={() => {
                                 localStorage.removeItem("token")
                                 localStorage.removeItem("user")
-                                window.location.href = "/login"
+                                 navigate("/login");
                             }}
                             className="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600 transition"
                         >
