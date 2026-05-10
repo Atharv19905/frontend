a8cf823d18ed37cb97ea3d31fbd7fac35ba7307c
Atharv19905 <atharvpchougule19@gmail.com>
2026-05-10 14:26:35 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    dedupe: ['react', 'react-dom'],
-    alias: {
-      'react': path.resolve('./node_modules/react'),
-      'react-dom': path.resolve('./node_modules/react-dom'),
-    }
-  },
-  build: {
-    minify: false,
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
-          'ui-vendor': ['framer-motion', 'react-hot-toast', 'react-select', 'react-icons'],
-          'chart-vendor': ['chart.js', 'react-chartjs-2', 'recharts'],
-          'pdf-vendor': ['jspdf', 'jspdf-autotable'],
-        }
-      }
-    }
+    dedupe: ['react', 'react-dom']
   }
 })
